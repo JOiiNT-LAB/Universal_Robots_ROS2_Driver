7fbeaab0078aefb52ca8152005cc52915a458ed1
mariocorsanici <mario.corsanici@gmail.com>
2024-10-28 15:08:22 +0100

    removed robot_kinematic and geometry_lib libraries

--- ur_robot_driver/src/ur10e_position_control.cpp
+++ ur_robot_driver/src/ur10e_position_control.cpp
@@ -5,9 +5,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 
-#include "robot_kinematic/robot_kinematic.hpp" // Ensure this header is properly included
-#include "geometry_utils/geometry_utils.hpp" // Ensure this header is properly included
+#include "geometry_lib.hpp" 
+#include "robot_kinematic.hpp" 
 
 using namespace std::chrono_literals;
 
@@ -29,8 +30,9 @@ public:
         auto description_parameter = parameters_client->get_parameters({"robot_description"});
         std::string robot_description_ = description_parameter[0].as_string();
 
-        std::string root_name = "base_link_inertia";
-        std::string tip_name = "wrist_3_link";
+        std::string             root_name = "base_link_inertia";
+        std::string             tip_name = "wrist_3_link";
+
         
         ur_kin_ = std::make_shared<RobotKinematic>(robot_description_, root_name, tip_name);
 
@@ -50,7 +52,93 @@ public:
         );
     }
 
+
+    void run()
+    {
+            switch (state_)
+            {
+                case 0:
+                {
+                    if(state_ != old_state_)
+                    {
+                        RCLCPP_INFO(this->get_logger(), "node state: %s", "IDLE");
+
+                        // Stop the info prompt in the run function
+                        old_state_ = state_;
+                    }
+
+                    state_ = 1;
+
+
+                    break;
+                }
+                case 1:
+                {
+                    if(state_ != old_state_)
+                    {
+                        RCLCPP_INFO(this->get_logger(), "node state: %s", "INIT");
+
+                        // Stop the info prompt in the run function
+                        old_state_ = state_;
+                    }
+
+
+                    // The init function is executed only if the position control 
+                    // received the first joint state message from the robot
+                    if(joint_state_obtained_)
+                    {
+                        init();
+                        state_ = 2;
+                    }
+                    break;
+                }
+                case 2:
+                {
+                    if(state_ != old_state_)
+                    {
+                        RCLCPP_INFO(this->get_logger(), "node state: %s", "UPDATE");
+
+                        // Stop the info prompt in the run function
+                        old_state_ = state_;
+                    }
+                    update();
+                    break;
+                }
+            }
+    }
+
+    void init()
+    {
+        desired_pos_  = current_pos_;
+        desired_quat_ = current_quat_;
+    }
+
+    void update()
+    {
+        // ur_kin_->ComputeDirectKinematic(ur_kin_->q_measured);
+        joint_command_ = ur_kin_->ComputeInverseKinematic(desired_pos_,desired_quat_);
+        
+        // joint_command_msg_ = 
+
+
+    }
+
 private:
+
+    // Functional variables
+    int state_ = 0;
+    int old_state_ = -1;
+
+    bool first_spin_ = true;
+    bool joint_state_obtained_ = false;
+
+    // KDL Variables
+    KDL::JntArray joint_command_;
+
+    // ROS2 Messages
+    std_msgs::msg::Float64MultiArray joint_command_msg_;
+    geometry_msgs::msg::PoseStamped current_pose_msg_;
+
     // ROS2 Publisher
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr current_pose_pub_;
 
@@ -58,42 +146,83 @@ private:
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_sub_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_des_sub_;
 
-    // Kinematic model
+    // Robot kinematic model
+    Eigen::Vector3d         current_pos_;
+    Eigen::Quaterniond      current_quat_;
+
+    Eigen::Vector3d         desired_pos_;
+    Eigen::Quaterniond      desired_quat_;
+
     std::shared_ptr<RobotKinematic> ur_kin_;
 
     // Callback function for joint states
     void jointStateCallback(sensor_msgs::msg::JointState::UniquePtr joint_state_msg) 
     {       
-        ur_kin_->q_measured_(0) = joint_state_msg->position[5];
-        ur_kin_->q_measured_(1) = joint_state_msg->position[0];
-        ur_kin_->q_measured_(2) = joint_state_msg->position[1];
-        ur_kin_->q_measured_(3) = joint_state_msg->position[2];
-        ur_kin_->q_measured_(4) = joint_state_msg->position[3];
-        ur_kin_->q_measured_(5) = joint_state_msg->position[4];
+        ur_kin_->q_measured(0) = joint_state_msg->position[5];
+        ur_kin_->q_measured(1) = joint_state_msg->position[0];
+        ur_kin_->q_measured(2) = joint_state_msg->position[1];
+        ur_kin_->q_measured(3) = joint_state_msg->position[2];
+        ur_kin_->q_measured(4) = joint_state_msg->position[3];
+        ur_kin_->q_measured(5) = joint_state_msg->position[4];
 
         // Compute the direct kinematics
-        auto [pos, quat] = ur_kin_->ComputeDirectKinematic(ur_kin_->q_measured_);
+
+        std::tie(current_pos_, current_quat_) = ur_kin_->ComputeDirectKinematic(ur_kin_->q_measured);
 
         // Convert Eigen vector position and quaternion to geometry_msgs/PoseStamped
-        auto pose_msg = eigen_pose_to_pose_stamped(pos, quat, this->get_clock()->now(), "wrist_3_link");
+        current_pose_msg_ = eigen_pose_to_pose_stamped(current_pos_, current_quat_, this->get_clock()->now(), "wrist_3_link");
 
         // Publish the message to ROS
-        current_pose_pub_->publish(pose_msg);
+        current_pose_pub_->publish(current_pose_msg_);
+
+        joint_state_obtained_ = true;
     }
 
     // Callback function for desired pose
-    void poseDesCallback(geometry_msgs::msg::PoseStamped::UniquePtr pose_des_msg) 
+    void poseDesCallback(geometry_msgs::msg::PoseStamped::UniquePtr pose_desired_msg) 
     {
-        std::cout << "CALLBACK" << std::endl;
-        // Handle the desired pose logic here
+        if(state_ == 2)
+        {
+            std::tie(desired_pos_, desired_quat_) = pose_stamped_to_eigen_pose(*pose_desired_msg);
+        }
+        else
+        {
+            RCLCPP_WARN(this->get_logger(), "the position controller is NOT subscribed to the joint states topic or has NOT received the first message");
+        }
     }
+
 };
+
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<UrPositionControl>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+
+    // Create the executor
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
+
+    rclcpp::Rate rate(1);  // 400 Hz loop
+    
+    // Main loop mimicking ROS 1's spinOnce()
+    while (rclcpp::ok())
+    {
+        // Process any pending callbacks (like ROS 1's spinOnce)
+        node->run();
+        executor.spin_some();
+        
+        // Sleep to maintain the loop rate
+        rate.sleep();
+    }
+
+    
+    // // ROS2 node shutdown
+    rclcpp::shutdown();    
     return 0;
 }
+
+//  ######################################################################################
+//     ERROR: if you have got the model from the parameter server and then get stuck in 
+//     the driver crashes, the node cannot get another joint state topic    
+//  ######################################################################################
